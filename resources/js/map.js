77599270c74c9184cc8e810c63843e95a71e79e0
nishenp <nishenkpeiris@gmail.com>
2018-10-29 08:57:21 +0530

    Sri Lanka should be centered on the map

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,29 +1,29 @@
 //Set up some of our variables.
 var map; //Will contain map object.
 var marker = false; ////Has the user plotted their location marker? 
-        
+
 //Function called to initialize / create the map.
 //This is called when the page has loaded.
 function initMap() {
 
     //The center location of our map.
-    var centerOfMap = new google.maps.LatLng(52.357971, -6.516758);
+    var centerOfMap = new google.maps.LatLng(7.8855063, 80.6395089);
 
     //Map options.
     var options = {
-      center: centerOfMap, //Set center.
-      zoom: 7 //The zoom value.
+        center: centerOfMap, //Set center.
+        zoom: 7 //The zoom value.
     };
 
     //Create the map object.
     map = new google.maps.Map(document.getElementById('map'), options);
 
     //Listen for any clicks on the map.
-    google.maps.event.addListener(map, 'click', function(event) {                
+    google.maps.event.addListener(map, 'click', function (event) {
         //Get the location that the user clicked.
         var clickedLocation = event.latLng;
         //If the marker hasn't been added.
-        if(marker === false){
+        if (marker === false) {
             //Create the marker.
             marker = new google.maps.Marker({
                 position: clickedLocation,
@@ -31,10 +31,10 @@ function initMap() {
                 draggable: true //make it draggable
             });
             //Listen for drag events!
-            google.maps.event.addListener(marker, 'dragend', function(event){
+            google.maps.event.addListener(marker, 'dragend', function (event) {
                 markerLocation();
             });
-        } else{
+        } else {
             //Marker has already been added, so just change its location.
             marker.setPosition(clickedLocation);
         }
@@ -42,17 +42,17 @@ function initMap() {
         markerLocation();
     });
 }
-        
+
 //This function will get the marker's current location and then add the lat/long
 //values to our textfields so that we can save the location.
-function markerLocation(){
+function markerLocation() {
     //Get location.
     var currentLocation = marker.getPosition();
     //Add lat and lng values to a field that we can save.
     document.getElementById('lat').value = currentLocation.lat(); //latitude
     document.getElementById('lng').value = currentLocation.lng(); //longitude
 }
-        
-        
+
+
 //Load the map when the page has finished loading.
 google.maps.event.addDomListener(window, 'load', initMap);
